ac9bef90bb86dd30d5245312ce9081774cff453d
Cyrus Yip <cyruscmyip1@gmail.com>
2021-02-15 15:52:47 -0800

    Use space and escape buttons for scene change

--- Menus/InGameGUI.gd
+++ Menus/InGameGUI.gd
@@ -1,4 +1,8 @@
 extends Control
 
+func _process(delta):
+	if Input.is_action_just_pressed("escape"):
+		get_tree().change_scene("res://Menus/Menu.tscn")	
+
 func _on_Button_pressed():
 	get_tree().change_scene("res://Menus/Menu.tscn")
--- Menus/Menu.gd
+++ Menus/Menu.gd
@@ -1,4 +1,8 @@
 extends Control
 
+func _process(delta):
+	if Input.is_action_just_pressed("space"):
+		Global.loadLevel()
+
 func _on_Button_pressed():
 	Global.loadLevel()
